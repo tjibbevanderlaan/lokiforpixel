5cfc32b2ce7a80904d456f7cc85cdab682d1e436
Tjibbe van der Laan <tjibbevanderlaan@gmail.com>
2017-08-23 19:47:49 +0200

    Added quite and continue flag to add-apt and apt-get

--- lokicrouton.sh
+++ lokicrouton.sh
@@ -50,13 +50,13 @@ sudo chown root:root startelementary
 
 # Install git
 # https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
-sudo apt-get install git-all
+sudo apt-get -y -qq install git-all
 
 # Install Hugh Greenbergs ported Pixel touchpad driver
 # We need to build it for Xenial
 # https://github.com/hugegreenbug
 # http://marksolters.com/programming/2016/06/20/ubuntu-16.04-pixel.html
-sudo apt-get install build-essential libjsoncpp-dev libglib2.0-dev # build essentials
+sudo apt-get -y -qq install build-essential libjsoncpp-dev libglib2.0-dev # build essentials
 git clone https://github.com/hugegreenbug/libevdevc.git
 cd libevdevc
 make
@@ -77,13 +77,13 @@ sudo cp xorg-conf/40-touchpad.conf /usr/share/X11/xorg.conf.d/40-touchpad.conf
 cd ..
 sudo cp preferences/touchpad/50-touchpad-cmt-peppy.conf /usr/share/X11/xorg.conf.d/50-touchpad.conf
 sudo mv /usr/share/X11/xorg.conf.d/50-synaptics.conf /usr/share/X11/xorg.conf.d/50-synaptics.conf-old
-sudo apt-get remove build-essential libjsoncpp-dev libglib2.0-dev # remove build essentials
+sudo apt-get -y -qq remove build-essential libjsoncpp-dev libglib2.0-dev # remove build essentials
 
 # Install Arc dark theme
 # https://github.com/horst3180/arc-theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
-sudo apt-get update
-sudo apt-get install arc-theme
+sudo apt-get -qq update
+sudo apt-get -y -qq install arc-theme
 
 # Plank in arc dark
 cd preferences
@@ -95,7 +95,7 @@ cd ..
 # NodeJS and NPM
 # https://nodejs.org/en/download/package-manager/
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get -y -qq install -y nodejs
 
 # Correct NPM permissions according to option 2
 # See docs: https://docs.npmjs.com/getting-started/fixing-npm-permissions
@@ -104,10 +104,10 @@ npm config set prefix '~/.npm-global'
 echo "export PATH=~/.npm-global/bin:$PATH" ~/.profile
 source ~/.profile
 
-# Sublime Text 3 + packages
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+# Sublime Text 3 + user packages
+sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get -qq update
+sudo apt-get -y -qq install sublime-text-installer
 cd preferences/sublime
 sudo cp "Package Control.sublime-settings" "~/.config/sublime-text-3/Installed Packages/"
 cd ../..
@@ -125,4 +125,4 @@ sudo cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
 cd ../..
 
 # You can run the chroot from the ChromeOs shell with the command:
-# 
+# sudo enter-chroot -n xiwi -X xorg startelementary
