95bc3f7732bd1d542e0b1ddbba7fb9fef88a4fcf
Tjibbe van der Laan <tjibbevanderlaan@gmail.com>
2017-08-24 21:08:14 +0200

    Tested each line from additional.sh, added a background

--- additional.sh
+++ additional.sh
@@ -8,20 +8,21 @@
 
 set -e
 
+# Install git
+# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+sudo apt-get -y -qq install git
+
+# Download preferences from repo
 cd /tmp/
 git clone https://github.com/tjibbevanderlaan/lokiforpixel.git
 cd /tmp/lokiforpixel
-
-
-# Install git
-# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
-sudo apt-get -y -qq install git-all
 
 # Install Hugh Greenbergs ported Pixel touchpad driver
 # We need to build it for Xenial
 # https://github.com/hugegreenbug
 # http://marksolters.com/programming/2016/06/20/ubuntu-16.04-pixel.html
-sudo apt-get -y -qq install build-essential libjsoncpp-dev libglib2.0-dev # build essentials
+sudo apt-get -y -qq install xutils-dev xserver-xorg-dev build-essential libjsoncpp-dev libglib2.0-dev # build essentials
+sudo apt-get -qq update
 git clone https://github.com/hugegreenbug/libevdevc.git
 cd /tmp/lokiforpixel/libevdevc
 make
@@ -38,23 +39,26 @@ cd /tmp/lokiforpixel/xf86-input-cmt
 make
 sudo make install
 sudo cp xorg-conf/20-mouse.conf /usr/share/X11/xorg.conf.d/20-mouse.conf
-sudo cp xorg-conf/40-touchpad.conf /usr/share/X11/xorg.conf.d/40-touchpad.conf
+sudo cp xorg-conf/40-touchpad-cmt.conf /usr/share/X11/xorg.conf.d/40-touchpad.conf
 cd /tmp/lokiforpixel
 sudo cp preferences/touchpad/50-touchpad-cmt-peppy.conf /usr/share/X11/xorg.conf.d/50-touchpad.conf
 sudo mv /usr/share/X11/xorg.conf.d/50-synaptics.conf /usr/share/X11/xorg.conf.d/50-synaptics.conf-old
-sudo apt-get -y -qq remove build-essential libjsoncpp-dev libglib2.0-dev # remove build essentials
+sudo apt-get -y -qq remove xutils-dev xserver-xorg-dev build-essential libjsoncpp-dev libglib2.0-dev # remove build essentials
 
 # Install Arc dark theme
 # https://github.com/horst3180/arc-theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
 sudo apt-get -qq update
-sudo apt-get -y -qq install arc-theme
+sudo apt-get -y -qq --allow-unauthenticated install arc-theme
 
 # Plank in arc dark
 cd /tmp/lokiforpixel/preferences
 wget https://raw.githubusercontent.com/horst3180/arc-theme/master/extra/Arc-Plank/dock.theme
 sudo cp dock.theme /usr/share/plank/themes
 cd /tmp/lokiforpixel
+
+# Install curl
+sudo apt-get -y -qq install curl
 
 ## App installation
 # NodeJS and NPM
@@ -70,9 +74,9 @@ echo "export PATH=~/.npm-global/bin:$PATH" ~/.profile
 source ~/.profile
 
 # Sublime Text 3 + user packages
-#sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
-#sudo apt-get -qq update
-#sudo apt-get -y -qq install sublime-text-installer
+sudo apt-add-repository -y ppa:webupd8team/sublime-text-3
+sudo apt-get -qq update
+sudo apt-get -y -qq install sublime-text-installer
 mkdir -p "~/.config/sublime-text-3/Installed Packages/"
 sudo cp "preferences/sublime/Package Controle.sublime-settings" "~/.config/sublime-text-3/Installed Packages/"
 
@@ -81,9 +85,9 @@ sudo cp "preferences/sublime/Package Controle.sublime-settings" "~/.config/subli
 cd /tmp/lokiforpixel
 git clone "https://github.com/GarthTheChicken/Sublime-Text-3-Arc-Dark-theme"
 mkdir -p "~/.config/sublime-text-3/Packages/User/"
-sudo cp Sublime-Text-3-Arc-Dark-theme/Arc-Dark ~/.config/sublime-text-3/Packages/User/
+sudo cp -r Sublime-Text-3-Arc-Dark-theme/Arc-Dark ~/.config/sublime-text-3/Packages/User/
 sudo cp Sublime-Text-3-Arc-Dark-theme/Widget.sublime-settings ~/.config/sublime-text-3/Packages/User/
-sudo cp papreferences/sublime/Arc-Monokai.tmTheme ~/.config/sublime-text-3/Packages/User/Arc-Dark/schemes/
+sudo cp preferences/sublime/Arc-Monokai.tmTheme ~/.config/sublime-text-3/Packages/User/Arc-Dark/schemes/
 sudo cp preferences/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
 
 # You can run the chroot from the ChromeOs shell with the command:
